24790fbe59866fa53e9d4faefbf6800e71c6b76d
danilbnd <bondarev250695>
2021-09-22 16:16:45 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const board = document.querySelector('#board')
+const colors = ['#5e51cb', '#f10c9d', '#97f702', '#14eef2', '#4ff656', '#f0bd16', '#fd302d', '#faeaac']
+const SQUARES_NUMBER = 500
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+    square.addEventListener('mouseover', () => setColor(square))
+    square.addEventListener('mouseleave', () => removeColor(square))
+    board.append(square)
+}
+
+function setColor(element) {
+    const color = getRandomColor()
+    element.style.backgroundColor = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(element) {
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow = `0 0 2px #000`
+}
+
+function getRandomColor() {
+    const index = Math.floor(Math.random() * colors.length)
+    return colors[index]
+}
